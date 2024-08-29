f7b72f24a50132a423d8f6eda0c331ba6284647b
Doran-28 <116115557+Doran-28@users.noreply.github.com>
2024-08-29 11:38:02 -0400

    Added remove button

--- script.js
+++ script.js
@@ -3,14 +3,22 @@
 const myLibrary = [];
 const container = document.querySelector('.container');
 
-function Book(title, author, pages, read=false) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `The ${this.title} by ${this.author}, ${this.pages} pages, read status: ${read}`
+        if (read)
+            return `The ${this.title} by ${this.author}, ${this.pages} pages, this book is read`
+        else
+            return `The ${this.title} by ${this.author}, ${this.pages} pages, this book is not read`;
     }
+
+}
+
+Book.prototype.toggleReadStatus = function() {
+    this.read = !this.read;
 }
 
 function addBookToLibrary(book) {
@@ -18,19 +26,33 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks() {
+    container.querySelectorAll('p').forEach(p => p.remove());
     for (let i = 0; i < myLibrary.length; i++){
         book = document.createElement('p');
         book.textContent = myLibrary[i].info();
+
+        remove = document.createElement('button');
+        remove.textContent = "Remove book";
         container.appendChild(book);
+        book.appendChild(remove);
+
+        readStatus = document.createElement('button');
+        readStatus.textContent = "Toggle read status";
+        book.appendChild(readStatus);
+
+        remove.addEventListener('click', function() {
+            book.remove(); // Remove the book element when the button is clicked
+            // Optional: You might also want to remove the book from the `myLibrary` array
+            myLibrary.splice(i, 1); // Removes the book from the array
+            displayBooks(); // Re-display the updated list of books
+        });
+
+        readStatus.addEventListener('click', () =>{
+            book.toggleReadStatus();
+            display()
+        })
     }
 }
-
-
-addBookToLibrary(new Book("title1", "author", 100, false));
-addBookToLibrary(new Book("title1", "author", 100, false));
-addBookToLibrary(new Book("title1", "author", 100, false));
-addBookToLibrary(new Book("title1", "author", 100, false));
-displayBooks()
 
 
 const addBook = document.querySelector(".container > button");
@@ -42,4 +64,15 @@ addBook.addEventListener('click', () => {
 const close = document.querySelector(".close");
 close.addEventListener("click", () => {
     modal.style.display = 'none';
+});
+
+const submit = document.querySelector('.modal form');
+submit.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const read = document.getElementById('read');
+    addBookToLibrary(new Book(title.value, author.value, pages.value, read.checked));
+    displayBooks();
 });
